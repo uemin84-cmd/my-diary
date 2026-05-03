61e01dd630c0c6b12614728aa86d3d531c0d33ac
uemin84-cmd <uemin84@gmail.com>
2026-05-03 17:45:46 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE = 'diary-v4';
+const ASSETS = ['/', '/index.html', '/manifest.json'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
+self.addEventListener('notificationclick', e => {
+  e.notification.close();
+  e.waitUntil(clients.openWindow('/'));
+});
