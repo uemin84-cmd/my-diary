d11757a6ae547a88993dcde2c8dba599977ddf0a
uemin84-cmd <uemin84@gmail.com>
2026-05-03 21:27:12 +0900

    Update webhook.js

--- netlify/functions/webhook.js
+++ netlify/functions/webhook.js
@@ -1,15 +1,32 @@
 exports.handler = async (event) => {
-  const MAKE_URL = 'https://hook.eu1.make.com/r9flw5xs4wwp18hp9jno6wr1rsqohhvs';
-  
-  const res = await fetch(MAKE_URL, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: event.body
-  });
+  if (event.httpMethod === 'OPTIONS') {
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type'
+      },
+      body: ''
+    };
+  }
 
-  return {
-    statusCode: 200,
-    headers: { 'Access-Control-Allow-Origin': '*' },
-    body: JSON.stringify({ ok: true })
-  };
+  const MAKE_URL = 'https://hook.eu1.make.com/r9flw5xs4wwp18hp9jno6wr1rsqohhvs';
+
+  try {
+    await fetch(MAKE_URL, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: event.body
+    });
+    return {
+      statusCode: 200,
+      headers: { 'Access-Control-Allow-Origin': '*' },
+      body: JSON.stringify({ ok: true })
+    };
+  } catch (e) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: e.message })
+    };
+  }
 };
